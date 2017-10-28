99c83f62f98cb3b66ccf887db68665a68ec6554e
kettsun0123 <kyuji7123.development@gmail.com>
2017-10-28 21:42:17 +0900

    add realm

--- src/screen/MainScreen/MainScreen.js
+++ src/screen/MainScreen/MainScreen.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import Styles from './styles';
+import styles from './styles';
+import Realm from 'realm';
 
 export default class MainScreen extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { realm: null };
   }
 
+  /* Lifecycle */
+  componentWillMount() {
+    Realm.open({
+      schema: [{name: 'Dog', properties: {name: 'string'}}]
+    }).then(realm => {
+      realm.write(() => {
+        realm.create('Dog', {name: 'Rex'});
+      });
+      this.setState({ realm });
+    });
+  }
+
+  /* private methods */
+  
+
+
   render() {
+    const info = this.state.realm ? 'Number of dogs in this Realm: ' + this.state.realm.objects('Dog').length : 'Loading...';
+
     return (
-      <View style={Styles.container}>
-        <Text>Hello world!</Text>
+      <View style={styles.container}>
+        <Text>
+          {info}
+        </Text>
       </View>
     );
   }
